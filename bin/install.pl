3c3766a7e93e95353622269fe52418fc6dfe0a9c
mattf <mattf@3d104415-ff17-0410-8863-d5cf3c621b8a>
2006-07-08 01:44:53 +0000

    install.pl created - not functional

--- bin/install.pl
+++ bin/install.pl
@@ -1,30 +1,30 @@
 #!/usr/bin/perl
 
-# build_translation_www_files.pl
+# install.pl
 #
 # Copyright (C) 2006  Matt Florell <vicidial@gmail.com>    LICENSE: GPLv2
 #
 
 ############################################
-# install_server_files.pl - puts server files in the right places
+# install.pl - puts server files in the right places and creates conf file
 #
 # modify the variables below to customize for your system.
 #
-# path to home directory: (assumes it already exists)
-$home =		'/home/cron';
+# default path to home directory:
+$home =		'/usr/share/astguiclient';
 
-# path to agi-bin directory: (assumes it already exists)
+# default path to agi-bin directory: 
 $agibin =	'/var/lib/asterisk/agi-bin';
 
-# path to web root directory: (assumes it already exists)
+# default path to web root directory: 
 #$webroot =	'/var/www/html';
 #$webroot =	'/home/www/htdocs';
 $webroot =	'/usr/local/apache2/htdocs';
 
-# path to asterisk sounds directory: (assumes it already exists)
+# default path to asterisk sounds directory: 
 $sounds =	'/var/lib/asterisk/sounds';
 
-# path to asterisk recordings directory: (assumes it already exists)
+# default path to asterisk recordings directory: 
 $monitor =	'/var/spool/asterisk';
 
 ############################################
@@ -278,233 +278,3 @@ print "\n\nDONE and EXITING\n";
 
 
 exit;
-
-
-
-sub translate_pages
-{
-#***LANGUAGES***
-#en-English|es-Espaol|
-#***FILES***
-#agc/astguiclient.php
-#agc/vicidial.php
-#***TRANSLATIONS***
-#English|Ingls|
-#Spanish|Espaol|
-
-##### PARSE THE LANGUAGES SETTING FILE #####
-$section='';
-$i=0;
-$Lct=0; $Fct=0; $Tct=0;
-foreach(@lang)
-	{
-	if ($lang[$i] !~ /^\#/)
-		{
-		if ($lang[$i] =~ /^\*\*\*/) 
-			{
-			$section = $lang[$i];
-			$section =~ s/\*|\n|\r//gi;
-		#	print "section heading: $section: $lang[$i]";
-			}
-		else
-			{
-			if ($section =~ /LANGUAGES/)
-				{
-				$LANGUAGES = $lang[$i];
-				$Lct++;
-				}
-			else
-				{
-				if ($section =~ /FILES/)
-					{
-				#	print "section: $section    line: $lang[$i]";
-					$FILES[$Fct] = $lang[$i];
-					$Fct++;
-					}
-				else
-					{
-					if ($section =~ /TRANSLATIONS/)
-						{
-					#	print "section: $section    line: $lang[$i]";
-						@TRANSlineX = split(/\|/, $lang[$i]);
-						$ORIGtextX = "$TRANSlineX[0]";
-						$lengthX = length($ORIGtextX);
-						$lengthX = sprintf("%04d",$lengthX);
-						$TRANSLATIONS_RAW[$Tct] = "$lengthX|$lang[$i]";
-						$Tct++;
-						}
-					}
-				}
-			}
-		}
-	$i++;
-	}
-
-if ($DB)
-	{
-	print "LANGUAGE FILE PARSE RESULTS:   $#lang lines in file\n";
-	print "LANGUAGES:    $LANGUAGES\n";
-	print "FILES:        $Fct\n";
-	print "TRANSLATIONS: $Tct\n";
-	}
-
-#@TRANSLATIONS = sort { length($b) <=> length($a) } @TRANSLATIONS_RAW;
-@TRANSLATIONS = sort { $b <=> $a } @TRANSLATIONS_RAW;
-
-#$k=0;
-#foreach(@TRANSLATIONS)
-#	{
-#	$length = length($TRANSLATIONS[$k]);
-#	print "$TRANSLATIONS[$k]|$length|\n";  
-#	$k++;
-#	}
-
-##### LOOP THROUGH THE LANGUAGES
-@lang_list = split(/\|/, $LANGUAGES);
-@lang_link_list = @lang_list;
-$i=0;
-$gif = '.gif';
-foreach(@lang_list)
-	{
-	if (length($lang_list[$i]) > 2) 
-		{
-		@lang_detail  = split(/-/, $lang_list[$i]);
-		$lang_abb = $lang_detail[0];
-		$lang_name = $lang_detail[1];
-
-		if ( ($without_en==1) && ($lang_abb =~ /en/) )
-			{
-			print "SKIPPING ENGLISH COPYING: $lang_abb|$i\n\n";
-			}
-		else
-			{
-			##### LOOP THROUGH THE FILES
-			$Fct=0;
-			foreach(@FILES)
-				{
-				$a=0;
-				@file_detail  = split(/\|/, $FILES[$Fct]);
-				$file_path = $file_detail[0];
-				$file_name = $file_detail[1];
-				$file_name =~ s/\t|\r|\n//gi;
-				$file_passthru = $file_detail[2];
-				$file_passthru =~ s/\t|\r|\n//gi;
-				if (-e "$webroot/$file_path/$file_name")
-					{
-					open(file, "$webroot/$file_path/$file_name") || die "can't open $webroot/$file_path/$file_name: $!\n";
-					@file = @MT;
-					@file = <file>;
-					close(file);
-
-					print "File exists: ./$file_path/$file_name\n";
-					if (-e "$webroot/$file_path$US$lang_abb")
-						{
-						print "Lang Directory exists: $webroot/$file_path$US$lang_abb\n";
-						}
-					else
-						{
-						`mkdir $webroot/$file_path$US$lang_abb`;
-						`chmod 0777 $webroot/$file_path$US$lang_abb`;
-						print "Lang Directory created: $webroot/$file_path$US$lang_abb\n";
-						}
-
-					if ($file_passthru < 1)
-						{
-						##### LOOP THROUGH THE TRANSLATIONS
-						$Tct=0;
-						foreach(@TRANSLATIONS)
-							{
-							@TRANSline = split(/\|/, $TRANSLATIONS[$Tct]);
-							$ORIGtext = "$TRANSline[1]";
-							$TRANStext = "$TRANSline[$i+1]";
-							$LINESct=0;
-							foreach(@file)
-								{
-								if ($file[$LINESct] !~ /^\#|^\s*function |'INCALL'|'PAUSED'|'READY'|'NEW'|xmlhttp|ILPV |ILPA |SELECT cmd_line_f|=\"SELECT/)
-									{
-									if ($file[$LINESct] =~ /INTERNATIONALIZATION-LINKS-PLACEHOLDER-VICIDIAL/)
-										{
-										$file[$LINESct] =~ s/INTERNATIONALIZATION-LINKS-PLACEHOLDER-VICIDIAL/ILPV/g;
-										$e=0;
-										$gif = '.gif';
-										foreach(@lang_link_list)
-											{
-											if (length($lang_link_list[$e]) > 2) 
-												{
-												@lang_list_detail  = split(/-/, $lang_link_list[$e]);
-												$lang_list_abb = $lang_list_detail[0];
-												$lang_list_name = $lang_list_detail[1];
-												if ($lang_list_abb =~ /$lang_abb/) {$list_bgcolor = ' BGCOLOR=\"#CCFFCC\"';}
-												else {$list_bgcolor = '';}
-												$file[$LINESct] .= "echo \"<TD WIDTH=100 ALIGN=RIGHT VALIGN=TOP $list_bgcolor NOWRAP><a href=\\\"../$file_path$US$lang_list_abb/$file_name?relogin=YES&VD_login=\$VD_login&VD_campaign=\$VD_campaign&phone_login=\$phone_login&phone_pass=\$phone_pass&VD_pass=\$VD_pass\\\">$lang_list_name <img src=\\\"../agc/images/$lang_list_abb$gif\\\" BORDER=0 HEIGHT=14 WIDTH=20></a></TD>\\n\";";
-												}
-											$e++;
-											}
-										}
-									else
-										{
-										if ($file[$LINESct] =~ /INTERNATIONALIZATION-LINKS-PLACEHOLDER-AGC/)
-											{
-											$file[$LINESct] =~ s/INTERNATIONALIZATION-LINKS-PLACEHOLDER-AGC/ILPA/g;
-											$e=0;
-											$gif = '.gif';
-											foreach(@lang_link_list)
-												{
-												if (length($lang_link_list[$e]) > 2) 
-													{
-													@lang_list_detail  = split(/-/, $lang_link_list[$e]);
-													$lang_list_abb = $lang_list_detail[0];
-													$lang_list_name = $lang_list_detail[1];
-													if ($lang_list_abb =~ /$lang_abb/) {$list_bgcolor = ' BGCOLOR=\"#CCFFCC\"';}
-													else {$list_bgcolor = '';}
-													$file[$LINESct] .= "echo \"<TD WIDTH=100 ALIGN=RIGHT VALIGN=TOP $list_bgcolor NOWRAP><a href=\\\"../$file_path$US$lang_list_abb/$file_name?relogin=YES&user=\$user&pass=\$pass&phone_login=\$phone_login&phone_pass=\$phone_pass\\\">$lang_list_name <img src=\\\"../agc/images/$lang_list_abb$gif\\\" BORDER=0 HEIGHT=14 WIDTH=20></a></TD>\\n\";";
-													}
-												$e++;
-												}
-											}
-										else
-											{
-											if ( ($lang_abb =~ /^en/) && ($Tct > 2) )
-												{
-												$file[$LINESct] =~ s/\.\/images\//\.\.\/agc\/images\//g;
-												$file[$LINESct] =~ s/\"help.gif/\"..\/astguiclient\/help.gif/g;
-												}
-											if ($lang_abb !~ /^en/)
-												{$file[$LINESct] =~ s/$ORIGtext/$TRANStext/g;}
-											}
-										}
-									}
-								$LINESct++;
-
-								if ($a =~ /00000$/i) {print "$a     $file[$LINESct]\n|$ORIGtext|$TRANStext|";}
-								$a++;
-								}
-							$Tct++;
-							}
-						}
-					### open the translation result file for writing ###
-					open(out, ">$webroot/$file_path$US$lang_abb/$file_name")
-							|| die "Can't open $webroot/$file_path$US$lang_abb/$file_name: $!\n";
-			#		print out "Header(\"Content-type: text/html; charset=utf-8\");\n";
-					$LINESct=0;
-					foreach(@file)
-						{
-						print out "$file[$LINESct]";
-						$LINESct++;
-						}
-					close(out);
-					print "\n";
-					print "File Written: $webroot/$file_path$US$lang_abb/$file_name\n";
-					}
-				else
-					{
-					print "File does not exist: $webroot/$file_path/$file_name\n";
-					}
-				$Fct++;
-				}
-			}
-		}
-	$i++;
-	}
-
-}
