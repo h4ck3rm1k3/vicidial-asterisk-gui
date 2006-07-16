0f409f4964248a97adb275343d2d53248bc43b23
mattf <mattf@3d104415-ff17-0410-8863-d5cf3c621b8a>
2006-07-16 03:27:03 +0000

    AST_agent_week.pl changed to use /etc/astguiclient.conf for config options

--- bin/AST_agent_week.pl
+++ bin/AST_agent_week.pl
@@ -8,6 +8,7 @@
 #
 # CHANGES
 # 60517-1100 - First version
+# 60715-2325 - changed to use /etc/astguiclient.conf for configs
 #
 
 $txt = '.txt';
@@ -151,36 +152,63 @@ $TLoutfile .= "TODAY:          $shipdate\n";
 $TLoutfile .= "THIS WEEK:  week $START_week    $STARTtarget_date - $ENDtarget_date\n";
 $TLoutfile .= "LAST WEEK:  week $PAST_START_week    $PAST_STARTtarget_date - $PAST_ENDtarget_date\n\n";
 
-#$h=0;
-#foreach(@PAST_date)
-#{
-#$TLoutfile .= "$PAST_day[$h] $PAST_date[$h]\n";
-#$h++;
-#}
 
-#print "$TLoutfile";
+# default path to astguiclient configuration file:
+$PATHconf =		'/etc/astguiclient.conf';
 
+open(conf, "$PATHconf") || die "can't open $PATHconf: $!\n";
+@conf = <conf>;
+close(conf);
+$i=0;
+foreach(@conf)
+	{
+	$line = $conf[$i];
+	$line =~ s/ |>|\n|\r|\t|\#.*|;.*//gi;
+	if ( ($line =~ /^PATHhome/) && ($CLIhome < 1) )
+		{$PATHhome = $line;   $PATHhome =~ s/.*=//gi;}
+	if ( ($line =~ /^PATHlogs/) && ($CLIlogs < 1) )
+		{$PATHlogs = $line;   $PATHlogs =~ s/.*=//gi;}
+	if ( ($line =~ /^PATHagi/) && ($CLIagi < 1) )
+		{$PATHagi = $line;   $PATHagi =~ s/.*=//gi;}
+	if ( ($line =~ /^PATHweb/) && ($CLIweb < 1) )
+		{$PATHweb = $line;   $PATHweb =~ s/.*=//gi;}
+	if ( ($line =~ /^PATHsounds/) && ($CLIsounds < 1) )
+		{$PATHsounds = $line;   $PATHsounds =~ s/.*=//gi;}
+	if ( ($line =~ /^PATHmonitor/) && ($CLImonitor < 1) )
+		{$PATHmonitor = $line;   $PATHmonitor =~ s/.*=//gi;}
+	if ( ($line =~ /^VARserver_ip/) && ($CLIserver_ip < 1) )
+		{$VARserver_ip = $line;   $VARserver_ip =~ s/.*=//gi;}
+	if ( ($line =~ /^VARDB_server/) && ($CLIDB_server < 1) )
+		{$VARDB_server = $line;   $VARDB_server =~ s/.*=//gi;}
+	if ( ($line =~ /^VARDB_database/) && ($CLIDB_database < 1) )
+		{$VARDB_database = $line;   $VARDB_database =~ s/.*=//gi;}
+	if ( ($line =~ /^VARDB_user/) && ($CLIDB_user < 1) )
+		{$VARDB_user = $line;   $VARDB_user =~ s/.*=//gi;}
+	if ( ($line =~ /^VARDB_pass/) && ($CLIDB_pass < 1) )
+		{$VARDB_pass = $line;   $VARDB_pass =~ s/.*=//gi;}
+	if ( ($line =~ /^VARDB_port/) && ($CLIDB_port < 1) )
+		{$VARDB_port = $line;   $VARDB_port =~ s/.*=//gi;}
+	$i++;
+	}
+
+# Customized Variables
+$server_ip = $VARserver_ip;		# Asterisk server IP
 
 $outfile = "AGENT_HOURS_$PAST_STARTtarget_date$US$PAST_ENDtarget_date$txt";
 
-#$dir = '/usr/local/apache2/htdocs';
-$dir = '/home/www/htdocs';
-
 ### open the X out file for writing ###
-open(out, ">$dir/vicidial/agent_reports/$outfile")
+open(out, ">$PATHweb/vicidial/agent_reports/$outfile")
 		|| die "Can't open $outfile: $!\n";
 
-### Make sure this file is in a libs path or put the absolute path to it
-require("/home/cron/AST_SERVER_conf.pl");	# local configuration file
+if (!$VARDB_port) {$VARDB_port='3306';}
 
-if (!$DB_port) {$DB_port='3306';}
 
 use DBI;	  
 
-$dbhA = DBI->connect("DBI:mysql:$DB_database:$DB_server:$DB_port", "$DB_user", "$DB_pass")
+$dbhA = DBI->connect("DBI:mysql:$VARDB_database:$VARDB_server:$VARDB_port", "$VARDB_user", "$VARDB_pass")
  or die "Couldn't connect to database: " . DBI->errstr;
 
-$dbhB = DBI->connect("DBI:mysql:$DB_database:$DB_server:$DB_port", "$DB_user", "$DB_pass")
+$dbhB = DBI->connect("DBI:mysql:$VARDB_database:$VARDB_server:$VARDB_port", "$VARDB_user", "$VARDB_pass")
  or die "Couldn't connect to database: " . DBI->errstr;
 
 #$vicidial_agent_log = 'vicidial_agent_log_archive';
@@ -299,20 +327,6 @@ foreach(@PAST_date)
 		$rec_count++;
 		}
 	$sthA->finish();
-
-
-
-
-
-
-
-
-
-
-#if ($h=='1')
-#	{
-#	exit;
-#	}
 
 $h++;
 }
