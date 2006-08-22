a19957b3709222824297ad61f9972e409b97dffc
mattf <mattf@3d104415-ff17-0410-8863-d5cf3c621b8a>
2006-08-22 08:40:07 +0000

    changed phone_number to be VARCHAR(12) to allow for Sweden phone numbers

--- extras/upgrade_2.0.1.sql
+++ extras/upgrade_2.0.1.sql
@@ -4,3 +4,11 @@ ALTER TABLE servers ADD agi_output ENUM('NONE','STDERR','FILE','BOTH') default '
 ALTER TABLE vicidial_campaigns ADD allcalls_delay SMALLINT(3) UNSIGNED default '0';
 ALTER TABLE vicidial_campaigns ADD omit_phone_code ENUM('Y','N') default 'N';
 ALTER TABLE vicidial_campaigns MODIFY campaign_recording ENUM('NEVER','ONDEMAND','ALLCALLS','ALLFORCE') default 'ONDEMAND';
+
+ALTER TABLE vicidial_list MODIFY phone_number VARCHAR(12);
+ALTER TABLE vicidial_auto_calls MODIFY phone_number VARCHAR(12);
+ALTER TABLE vicidial_log MODIFY phone_number VARCHAR(12);
+ALTER TABLE vicidial_closer_log MODIFY phone_number VARCHAR(12);
+ALTER TABLE vicidial_xfer_log MODIFY phone_number VARCHAR(12);
+ALTER TABLE vicidial_list_pins MODIFY phone_number VARCHAR(12);
+ALTER TABLE vicidial_dnc MODIFY phone_number VARCHAR(12);
