0f83156bfd1e797b9f1b2790cc964b69b7a8ab34
mattf <mattf@3d104415-ff17-0410-8863-d5cf3c621b8a>
2006-09-07 15:44:09 +0000

    fixed some display issues with timeonVDAD scripts

--- www/vicidial/AST_timeonVDADall_SIPmonitor.php
+++ www/vicidial/AST_timeonVDADall_SIPmonitor.php
@@ -240,7 +240,9 @@ $talking_to_print = mysql_num_rows($rslt);
 			{
 			$row[5]=$row[6];
 			}
-		$extension =		sprintf("%-10s", $row[0]);
+		$extension = eregi_replace('Local/',"",$row[0]);
+		$extension =		sprintf("%-10s", $extension);
+			while(strlen($extension)>10) {$extension = substr("$extension", 0, -1);}
 		$user =				sprintf("%-6s", $row[1]);
 		$sessionid =		sprintf("%-9s", $row[2]);
 		$status =			sprintf("%-6s", $row[3]);
