297c226bf37f089b19e976e7802b205049dff1fe
frankw <frankw@3d104415-ff17-0410-8863-d5cf3c621b8a>
2008-06-26 09:23:33 +0000

    updated to get more things from current places

--- SuSE/get_sources.sh
+++ SuSE/get_sources.sh
@@ -7,9 +7,13 @@ wget http://superb-west.dl.sourceforge.net/sourceforge/ploticus/pl240src.tar.gz
 wget http://bart.eaccelerator.net/source/0.9.5.2/eaccelerator-0.9.5.2.tar.bz2
 wget http://superb-east.dl.sourceforge.net/sourceforge/mtop/mtop-0.6.6.tar.gz
 wget http://download.berlios.de/sipsak/sipsak-0.9.6-1.tar.gz
-wget http://ftp.digium.com/pub/asterisk/releases/asterisk-1.2.27.tar.gz
+wget http://ftp.digium.com/pub/asterisk/releases/asterisk-1.2.29.tar.gz
 wget http://ftp.digium.com/pub/zaptel/releases/zaptel-1.2.24.tar.gz
+#wget http://ftp.digium.com/pub/zaptel/releases/zaptel-1.2.26.tar.gz
 wget http://ftp.digium.com/pub/libpri/releases/libpri-1.2.7.tar.gz
+#wget http://downloads.digium.com/pub/asterisk/releases/asterisk-1.4.19.1.tar.gz
+#wget http://downloads.digium.com/pub/libpri/libpri-1.4.3.tar.gz
+#wget http://downloads.digium.com/pub/zaptel/zaptel-1.4.10.tar.gz
 
 mkdir patches
 cd patches
