18063dc0f00ad2f3d1e75843262163c4adeb5ab4
mattf <mattf@3d104415-ff17-0410-8863-d5cf3c621b8a>
2009-02-10 00:58:12 +0000

    updated new logo in several places

--- www/vicidial/welcome_demo.php
+++ www/vicidial/welcome_demo.php
@@ -9,7 +9,7 @@ header ("Content-type: text/html; charset=utf-8");
 echo "<title>VICIDIAL Welcome</title>\n";
 echo "</head>\n";
 echo "<BODY BGCOLOR=WHITE MARGINHEIGHT=0 MARGINWIDTH=0>\n";
-echo "<BR><BR><BR><BR><CENTER><TABLE WIDTH=600 CELLPADDING=0 CELLSPACING=0 BGCOLOR=\"#E0C2D6\"><TR BGCOLOR=WHITE>";
+echo "<BR><BR><BR><BR><CENTER><TABLE WIDTH=600 CELLPADDING=0 CELLSPACING=0 BGCOLOR=\"#CCCCCC\"><TR BGCOLOR=WHITE>";
 echo "<TD ALIGN=LEFT VALIGN=BOTTOM WIDTH=120><IMG SRC=\"../agc/images/vdc_tab_vicidial.gif\" BORDER=0></TD>";
 echo "<TD ALIGN=RIGHT VALIGN=MIDDLE WIDTH=180> Welcome! Bienvenue! &nbsp; </TD>";
 echo "<TD ALIGN=LEFT VALIGN=MIDDLE WIDTH=300> Willkommen! Benvenuto! Υποδοχή!</TD>";
@@ -17,7 +17,7 @@ echo "</TR>\n";
 echo "<TR><TD ALIGN=LEFT COLSPAN=3><font size=1> &nbsp; </TD></TR>\n";
 echo "<TR><TD ALIGN=CENTER COLSPAN=2 VALIGN=TOP>\n";
 
-echo "<TABLE WIDTH=300 CELLPADDING=2 CELLSPACING=0 BGCOLOR=\"#E0C2D6\">";
+echo "<TABLE WIDTH=300 CELLPADDING=2 CELLSPACING=0 BGCOLOR=\"#CCCCCC\">";
 echo "<TR><TD ALIGN=LEFT>";
 echo "<font size=3><b> &nbsp; <a href=\"../agc/vicidial.php?relogin=YES&VD_login=6666&VD_campaign=TESTCAMP&phone_login=phone&phone_pass=phone&VD_pass=1234\"><IMG SRC=\"../agc/images/en.gif\" border=0> English Agent Login</a>";
 echo "</TD></TR>\n";
@@ -51,7 +51,7 @@ echo "</TD></TR></TABLE>\n";
 
 echo "</TD><TD VALIGN=TOP>\n";
 
-echo "<TABLE WIDTH=300 CELLPADDING=2 CELLSPACING=0 BGCOLOR=\"#E0C2D6\">";
+echo "<TABLE WIDTH=300 CELLPADDING=2 CELLSPACING=0 BGCOLOR=\"#CCCCCC\">";
 echo "<TR><TD ALIGN=LEFT >";
 echo "<font size=3><b> &nbsp; <a href=\"../vicidial/admin.php\"><IMG SRC=\"../agc/images/en.gif\" border=0> English Administration</a>";
 echo "</TD></TR>\n";
